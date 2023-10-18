233a4ce0769391f84dfa8bcee500ba32d60a70fe
Caleb Weeks <me@sethcalebweeks.com>
2023-10-18 00:19:57 -0400

    Music player working!

--- src/components/AudioPlayer.svelte
+++ src/components/AudioPlayer.svelte
@@ -1,44 +1,54 @@
 <script>
-	import { onMount } from "svelte";
-  import { currentSong } from '../scripts/audioStore';
+	import { onMount, onDestroy } from "svelte";
+  import { currentSong, isPlaying } from '../scripts/audioStore';
 
   export let albums = {};
-  let player;
-  let trackName;
-  let albumName;
-  let src;
-	let isPlaying = false;
+  let player, album, track, src;
+  let unsubscribeCurrentSong, unsubscribeIsPlaying, trackIndex, currentTime;
 
-  console.log(albums)
+  $: [album, track] = $currentSong; 
+  $: trackIndex = albums[album].findIndex(({title}) => title === track);
+  $: src = albums[album][trackIndex].src;
 
-	onMount(() => {
-		currentSong.subscribe(([album, song]) => {
-      if (trackName !== song) {
-        albumName = album;
-        trackName = song;
-        src = "/music/" + albums[album][song]
-        // trackSource = albums[album].find()
-        player.load();
-        isPlaying = true;
-        player.play();
-      }
-    });
-	});
+  unsubscribeCurrentSong = currentSong.subscribe(() => {
+    if (unsubscribeCurrentSong) {
+      player.load();
+      player.play();
+    }
+  });
 
-	const togglePlay = () => {
-		isPlaying ? player.pause() : player.play();
-		isPlaying = !isPlaying;
-	};
+  unsubscribeIsPlaying = isPlaying.subscribe((isPlaying) => {
+    if (unsubscribeIsPlaying) {
+      isPlaying ? player.play() : player.pause();
+    }
+  })
+
+  onDestroy(() => {
+    unsubscribeCurrentSong();
+    unsubscribeIsPlaying();
+  });
+
+  const onEnded = () => {
+    trackIndex = trackIndex >= (albums[album].length - 1) ? 0 : trackIndex + 1;
+    track = albums[album][trackIndex].title;
+    currentSong.set([album, track]);
+  }
+
+	const togglePlay = () =>  {
+    isPlaying.set(!$isPlaying);
+  };
 </script>
 
-<audio bind:this={player} data-astro-transition-persist="player">
-	<source {src} type="audio/mp4" />
+<audio bind:this={player} bind:currentTime on:ended={onEnded}>
+	<source {src} type="audio/mp3" />
 </audio>
 
-<p>{$currentSong}</p>
+<p class="max-w-xs sm:max-w-none text-ellipsis whitespace-nowrap overflow-hidden px-1">
+  {album} | {track}
+</p>
 
 <button on:click={togglePlay}>
-  {#if isPlaying}
+  {#if $isPlaying}
     <slot name="pause" />
   {:else}
     <slot name="play" />
--- src/components/PlaySong.svelte
+++ src/components/PlaySong.svelte
@@ -1,17 +1,27 @@
 <script>
-  import { currentSong } from "../scripts/audioStore";
+  import { currentSong, isPlaying } from "../scripts/audioStore";
 
   export let album = "Merciful & Mighty";
   export let song = "Westminster Confession of God";
 
-  function playSong() {
-    const player = document.getElementById("player");
-    player.album = album;
-    player.track = song;
-    currentSong.set([album, song]);
+  let currentTrack;
+
+  $: [_, currentTrack] = $currentSong;
+
+  function togglePlay() {
+    if (currentTrack === song) {
+      isPlaying.set(!$isPlaying);
+    } else {
+      isPlaying.set(true);
+      currentSong.set([album, song]);
+    }
   }
 </script>
 
-<span on:click={playSong}>
-  <slot />
+<span on:click={togglePlay}>
+  {#if currentTrack === song && $isPlaying}
+    <slot name="pause" />
+  {:else}
+    <slot name="play" />
+  {/if}
 </span>
